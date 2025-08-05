b1659aaedd28ad597624414e551cc18112949d69
Marta <martadelfino23@gmail.com>
2025-08-05 16:47:20 +0100

    removed readr from step1 file

--- R/step1HgncGeneList.R
+++ R/step1HgncGeneList.R
@@ -8,10 +8,10 @@
 #' @export
 fetch_hgnc_gene_list <- function(save_raw = FALSE, save_path = NULL) {
   # Fetch hgnc gene file
-  protein_coding_genes <- readr::read_delim("https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/locus_types/gene_with_protein_product.txt",
-    delim = "\t",
-    col_names = TRUE,
-    col_select = c(hgnc_id, uniprot_ids, symbol, ensembl_gene_id, entrez_id)
+  protein_coding_genes <- data.table::fread("https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/locus_types/gene_with_protein_product.txt",
+    sep = "\t",
+    select = c("hgnc_id", "uniprot_ids", "symbol", "ensembl_gene_id",
+               "entrez_id")
   ) %>%
     as.data.frame()
 
@@ -20,7 +20,7 @@ fetch_hgnc_gene_list <- function(save_raw = FALSE, save_path = NULL) {
     if (is.null(save_path)) {
       save_path <- "data/hgnc_gene_list.csv"
     }
-    readr::write_csv(protein_coding_genes, save_path)
+    write.csv(protein_coding_genes, save_path, row.names = FALSE)
   }
 
   cat("\n(1/12) finished running hgnc_gene_list.R\n")
